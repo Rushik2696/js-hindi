ee0631ae4313af0b97666f92a024a47823ed5384
Rushik2696 <132152688+Rushik2696@users.noreply.github.com>
2023-11-12 04:14:04 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Rushik")
+
+let name = "Rushik"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// bolean => true/false
+// null =>standalone value
+// undefined =>
+// symbol =>unique
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
